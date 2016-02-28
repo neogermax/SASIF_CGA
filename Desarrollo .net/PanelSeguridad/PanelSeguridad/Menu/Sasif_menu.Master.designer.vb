7cdcefab26d7614d36a4934c4bbbe4c5f8006841
german rodriguez <neogermax@gmail.com>
2016-02-28 10:55:27 -0500

    pull  master
    subida del proyecto sasif

---
+++ Desarrollo .net/PanelSeguridad/PanelSeguridad/Menu/Sasif_menu.Master.designer.vb
@@ -0,0 +1,51 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class Sasif_menu
+
+    '''<summary>
+    '''Control head.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents head As Global.System.Web.UI.WebControls.ContentPlaceHolder
+
+    '''<summary>
+    '''Control form1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+
+    '''<summary>
+    '''Control Arbol_Sasif.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Arbol_Sasif As Global.System.Web.UI.WebControls.TreeView
+
+    '''<summary>
+    '''Control main.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents main As Global.System.Web.UI.WebControls.ContentPlaceHolder
+End Class
---
+++ Desarrollo .net/PanelSeguridad/PanelSeguridad/Menu/menu.js
@@ -0,0 +1,49 @@
+﻿var ArrayMenu = [];
+   
+
+$(document).ready(function () {
+
+    //capturamos la url
+    var URLPage = window.location.search.substring(1);
+    var URLVariables = URLPage.split('&');
+    var User = URLVariables[0].replace("User=", "");
+    $("#User").html(User.toUpperCase());
+
+    $("#tituloPrincipal").html("GESTION Y CONTROL DE ORDENES DE TRABAJO");
+
+    //traemos los datos
+    transacionAjax("consulta");
+
+    //funcion para las ventanas emergentes
+    $("#dialog").dialog({
+        autoOpen: false
+    });
+
+});
+
+//hacemos la transaccion al code behind por medio de Ajax
+function transacionAjax(State) {
+    $.ajax({
+        url: "menuAjax.aspx",
+        type: "POST",
+        //crear json
+        data: { "action": State,
+            "user": $("#User").html()
+        },
+        //mostrar resultados de la creacion de la idea
+        success: function (result) {
+            if (result == "") {
+                ArrayMenu = [];
+            }
+            else {
+                ArrayMenu = JSON.parse(result);
+            }
+        },
+        error: function () {
+            $("#dialog").dialog("option", "title", "Disculpenos :(");
+            $("#Mensaje_alert").text("Se genero error al realizar la transacción Ajax!");
+            $("#dialog").dialog("open");
+            $("#DE").css("display", "block");
+        }
+    });
+}
